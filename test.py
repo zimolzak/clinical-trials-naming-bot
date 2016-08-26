85605477454880238d1a1167046c9c063c59da31
Andrew J. Zimolzak <Andrew.Zimolzak@va.gov>
2016-08-26 13:52:18 -0400

    Further augment vocabulary and also test it for repeats.

--- test.py
+++ test.py
@@ -3,8 +3,17 @@
 from string import ascii_lowercase
 
 medical = open('medical.txt', 'r').read().splitlines()
-initials = list(map(lambda s: s[0], medical))
+positive = open('positive.txt', 'r').read().splitlines()
+
 print('Missing the following initials:')
+initials = list(map(lambda s: s[0], medical))
 for c in ascii_lowercase:
     if c not in initials:
         print(c, end='')
+print('\n')
+
+for i, txtfile in enumerate([medical, positive]):
+    print('The following are repeated in text file ' + str(i) + ':')
+    for w in txtfile:
+        if medical.count(w) > 1:
+            print(w)
